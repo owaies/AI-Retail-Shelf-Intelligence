ca60dcf7705430fcb9ffd830e9b3fff146c93a29
owaies <owaies786@gmail.com>
2026-09-11 12:42:21 +0530

    feat: expose Day 4 workspace pages

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,2 +1,3 @@
 export { AnalyticsPage, AnalyzePage, DashboardPage, HistoryPage, SettingsPage } from './WorkspacePages'
+export { Day4AnalyticsPage, Day4HistoryPage } from './Day4Insights'
 export { LoginPage } from './LoginPage'
